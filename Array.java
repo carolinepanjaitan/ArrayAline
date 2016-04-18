8be4cda8ff6c39f204febea4cdde35a08f8e7087
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-04-18 13:21:51 +0700

    Aline

---
+++ Array.java
@@ -0,0 +1,45 @@
+package array;
+
+public class Array {
+
+    public static void main(String[] args) {
+        String [] a = {"Apel", "Jeruk", "Mangga", "Markisa", "Melon", "Anggur", "Nanas", "Delima", "Belimbing", "Jambu"};
+        
+        
+        String [] b = new String[10];
+        b[0] = "Sedan";
+        b[1] = "Toyota";
+        b[2] = "Bmw";
+        b[3] = "Honda";
+        b[4] = "Yamaha";
+        b[5] = "Ducati";
+        b[6] = "Kawasaki";
+        b[7] = "Marcedes";
+        b[8] = "Kijang";
+        b[9] = "Kodok";
+        
+        
+        System.out.println("Hasil buah: " + " " +a[0]);
+        System.out.println("Hasil buah: " + " " +a[1]);
+        System.out.println("Hasil buah: " + " " +a[2]);
+        System.out.println("Hasil buah: " + " " +a[3]);
+        System.out.println("Hasil buah: " + " " +a[4]);
+        System.out.println("Hasil buah: " + " " +a[5]);
+        System.out.println("Hasil buah: " + " " +a[6]);
+        System.out.println("Hasil buah: " + " " +a[7]);
+        System.out.println("Hasil buah: " + " " +a[8]);
+        System.out.println("Hasil buah: " + " " +a[9]);
+        System.out.println(" ");
+        System.out.println("Hasil Kendaraan: " + " " +b[0]);
+        System.out.println("Hasil Kendaraan: " + " " +b[1]);
+        System.out.println("Hasil Kendaraan: " + " " +b[2]);
+        System.out.println("Hasil Kendaraan: " + " " +b[3]);
+        System.out.println("Hasil Kendaraan: " + " " +b[4]);
+        System.out.println("Hasil Kendaraan: " + " " +b[5]);
+        System.out.println("Hasil Kendaraan: " + " " +b[6]);
+        System.out.println("Hasil Kendaraan: " + " " +b[7]);
+        System.out.println("Hasil Kendaraan: " + " " +b[8]);
+        System.out.println("Hasil Kendaraan: " + " " +b[9]);
+    }
+    
+}
