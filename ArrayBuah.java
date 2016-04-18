1da8c4f84dfafd01614c9b627c9db3e78b1f4e16
carolinepanjaitan <carolinepanjaitan5@gmail.com>
2016-04-18 13:27:26 +0700

    Aline

---
+++ ArrayBuah.java
@@ -0,0 +1,33 @@
+package arraybuah;
+
+public class ArrayBuah {
+
+    public static void main(String[] args) {
+    int buah = 10;
+    String [] b = new String[10];
+    
+    b[0]= "apel";
+    b[1]= "jeruk";
+    b[2]= "mangga";
+    b[3]= "markisa";
+    b[4]= "alpuket";
+    b[5]= "anggur";
+    b[6]= "melon";
+    b[7]= "duku";
+    b[8]= "duren";
+    b[9]= "sirsak";
+    
+        System.out.println("1." +" "+b[0]);
+        System.out.println("2." +b[1]);
+        System.out.println("3." +b[2]);
+        System.out.println("4." +b[3]);
+        System.out.println("5." +b[4]);
+        System.out.println("6." +b[5]);
+        System.out.println("7." +b[6]);
+        System.out.println("8." +b[7]);
+        System.out.println("9." +b[8]);
+        System.out.println("10." +b[9]);
+        System.out.println(b.length);
+    }
+    
+}
